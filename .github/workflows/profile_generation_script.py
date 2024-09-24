d03dffddfb3c9c48662dbbb420a9ae80342ad1c8
Bernd Müller <bernd.mueller@zbmed.de>
2024-09-24 12:42:39 +0200

    corrected datetime printing

--- .github/workflows/profile_generation_script.py
+++ .github/workflows/profile_generation_script.py
@@ -331,9 +331,7 @@ def generate_spec_info(g, path_changed_file):
     spec_info["version"] = arg.split("/")[-1].split(".json")[0].split("v")[-1]
 
     spec_info["version_date"] = (
-        str(datetime.datetime.now().date().year)
-        + str(datetime.datetime.now().date().month)
-        + str(datetime.datetime.now().date().day)
+        str(datetime.today().strftime('%Y%m%d'))
         + "T"
         + str(datetime.datetime.now().time().hour)
         + str(datetime.datetime.now().time().minute)
